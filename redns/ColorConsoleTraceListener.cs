b626b5f1710e7393f246b19f9648996e9ef1949f
Luis Medel <luis@medel.email>
2022-04-05 21:58:59 +0200

    Added support for NO_COLOR environment variable

---
+++ redns/ColorConsoleTraceListener.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace redns
+{
+    class ColorConsoleTraceListener
+        : TextWriterTraceListener
+    {
+        public ColorConsoleTraceListener()
+            : base (Console.Out)
+        {
+        }
+
+        public override void WriteLine (string message)
+        {
+            if (string.IsNullOrEmpty (message) || !_colors.TryGetValue (message.Substring (0, 6), out ConsoleColor color))
+                color = ConsoleColor.Gray;
+
+            lock (_lockobj)
+            {
+                Console.ForegroundColor = color;
+                base.WriteLine (message);
+            }
+        }
+
+        static readonly Dictionary<string, ConsoleColor> _colors = new Dictionary<string, ConsoleColor> (StringComparer.InvariantCultureIgnoreCase) {
+            { "INFO  ", ConsoleColor.White },
+            { "NOTICE", ConsoleColor.DarkGreen },
+            { "WARN  ", ConsoleColor.DarkYellow },
+            { "ERROR ", ConsoleColor.DarkRed },
+        };
+
+        static readonly object _lockobj = new object ();
+    }
+}
